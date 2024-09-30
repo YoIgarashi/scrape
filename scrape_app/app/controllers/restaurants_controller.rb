38145b54281d5de03cffc51fce7f24f34948a746
YoIgarashi <m22y10m20.yoshikun@gmail.com>
2024-10-01 00:35:17 +0900

    create index page

--- scrape_app/app/controllers/restaurants_controller.rb
+++ scrape_app/app/controllers/restaurants_controller.rb
@@ -1,6 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
-    HotpepperScrapingService.scrape
-   render plain: ""
+    if Restaurant.count == 0
+      # レストランが存在しない場合のみスクレイピングを実行
+      HotpepperScrapingService.scrape
+    end
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
